146f35b05f2995c979dda33c4584b0590f98477f
Steelsoultether <skyridersam@yahoo.com>
2024-06-15 18:55:30 -0600

    Commit main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+# This will be the main file for the calculator app.
+# TODO
+#  1 - Implement basic math functions
+#  2 - Implement a GUI
+#  3 - Connect both
+#  4 - Create other tabs for specific formulas and fields.
+
